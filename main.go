8496bd8a21344f729a7f45a42917cda8874dd10d
Naoki Yoneyama <naoki.yoneyama.85@gmail.com>
2019-06-30 18:16:57 +0900

    Fix binary code

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 )
 
 func main() {
@@ -50,16 +51,22 @@ func skipReadingRow(text string) bool {
 
 func compileToBinary(str string) string {
 	if regexp.MustCompile(`^@\d`).MatchString(str) {
-		return "0000000000010000"
+		position, _ := strconv.Atoi(regexp.MustCompile(`\d+`).FindString(str))
+		binaryPosition := fmt.Sprintf("%b", position)
+		b := "0"
+		for i := 1; i < 16 - len(binaryPosition); i++ {
+			b = b + "0"
+		}
+		return b + binaryPosition
 	}
 	if str == "D=A" {
-		return "1110110000001000"
+		return "1110110000010000"
 	}
 	if str == "D=D+A" {
-		return "1110000010001000"
+		return "1110000010010000"
 	}
 	if str == "M=D" {
-		return "1111110000010000"
+		return "1110001100001000"
 	}
 	return str
 }
